1e10f1a2568718734f89962a21b2a83e9a2c87e9
chLi <565353780@qq.com>
2024-08-20 19:26:14 +0800

    add impulse noise

--- open3d_manage/Method/noise.py
+++ open3d_manage/Method/noise.py
@@ -6,6 +6,12 @@ from copy import deepcopy
 def getGaussNoise(mean: float, sigma: float, noise_shape: list) -> np.ndarray:
     return np.random.normal(mean, sigma, noise_shape)
 
+def getImpulseNoise(probability: float, noise_shape: list) -> np.ndarray:
+    noise = np.zeros(noise_shape, dtype=np.float32)
+
+    noise[np.random.rand(*noise_shape) < probability] = np.random.rand()
+    return noise
+
 
 def toGaussNoisePCD(
     pcd: o3d.geometry.PointCloud, mean: float, sigma: float
@@ -19,3 +25,15 @@ def toGaussNoisePCD(
 
     gauss_noise_pcd.points = o3d.utility.Vector3dVector(points)
     return gauss_noise_pcd
+
+ 
+def toImpulseNoisePCD(pcd: o3d.geometry.PointCloud, probability: float):
+    impulse_noise_pcd = deepcopy(pcd)
+ 
+    points = np.asarray(impulse_noise_pcd.points)
+
+    impulse_noise = getImpulseNoise(probability, points.shape)
+    points += impulse_noise
+
+    impulse_noise_pcd.points = o3d.utility.Vector3dVector(points)
+    return impulse_noise_pcd
