a2613f6f3cfdfae10f1481fa84d9e801dee15d46
chLi <565353780@qq.com>
2024-11-04 22:43:44 +0800

    remove output debug info

--- src/MeshSplit/mesh_spliter.cpp
+++ src/MeshSplit/mesh_spliter.cpp
@@ -47,7 +47,7 @@ const std::unordered_map<int, std::set<int>> MeshSpliter::splitMeshByCurvature(
 
   sub_mesh_manager.sortSubMeshIdxSetMap();
 
-  sub_mesh_manager.checkSubMeshState();
+  // sub_mesh_manager.checkSubMeshState();
 
   sub_mesh_manager.paintSubMesh();
   // sub_mesh_manager.renderSubMeshes();
