059fad7932761676638cb2b4e2dfd05aa2ecf4cf
chLi <565353780@qq.com>
2024-11-26 21:17:38 +0800

    fix compile bugs on new machine

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ elif [ "$(uname)" = "Linux" ]; then
   pip install -U open3d
 fi
 
-sudo apt install libcgal-dev -y
+sudo apt install libc++-dev libc++abi-dev libcgal-dev -y
 
 pip install -U numpy tqdm gradio plotly opencv-python
 
